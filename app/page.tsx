7b05e61e0db7c3b06cd67e2ded7c8604f6ad9bf6
gyo82v <gyo82v@gmail.com>
2026-09-20 16:16:23 +0100

    chore: update main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-bold text-red-600">media shelf app</h1>
+    <div >
+      <h1 >media shelf app</h1>
     </div>
   );
 }
