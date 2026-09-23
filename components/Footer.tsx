4108f698783b71f0b4bcf30f4b82f85bb23acb9c
gyo82v <gyo82v@gmail.com>
2026-09-23 14:08:53 +0100

    feat: update footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer (){
     return(
-        <footer>
-            <p>footer here</p>
+        <footer className={`p-4 flex items-center justify-center border-t border-gray-300`}>
+            <p className="text-gray-500">
+                © 2023 Media Shelf. All rights reserved.
+            </p>
         </footer>
     )
 }
